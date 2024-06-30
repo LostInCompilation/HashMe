14aca5230d1f1a22f4695543232f06729ff69229
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2024-06-30 18:59:47 +0200

    Updated CMake files, added Architecture option for VisualStudio.

--- generateVisualStudioProject.bat
+++ generateVisualStudioProject.bat
@@ -4,14 +4,16 @@ IF "%~1" == "" GOTO HELP_TEXT
 
 set "BUILD_TYPE=%~1"
 set "TARGET_IDE=%~2"
+set "ARCHITECTURE=%~3"
 
-cmake -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -G "%TARGET_IDE%" -S . -B projects/ --fresh
+cmake -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
 
 exit
 
 :HELP_TEXT
 echo Usage:
 echo.
-echo generateVisualStudioProject.bat BUILD_TYPE VISUAL_STUDIO_VERSION
+echo generateVisualStudioProject.bat BUILD_TYPE VISUAL_STUDIO_VERSION ARCHITECTURE
 echo     BUILD_TYPE: Debug, Release
 echo     VISUAL_STUDIO_VERSION: "Visual Studio 17 2022" or "Visual Studio 16 2019" or older...
+echo	 ARCHITECTURE: Win32 x64 ARM ARM64
