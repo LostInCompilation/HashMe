8e5d173bb9fd452622b968b6f21e0e550c323a2c
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2024-06-30 19:15:08 +0200

    Updated VisualStudio generation script.

--- generateVisualStudioProject.bat
+++ generateVisualStudioProject.bat
@@ -6,7 +6,8 @@ set "BUILD_TYPE=%~1"
 set "TARGET_IDE=%~2"
 set "ARCHITECTURE=%~3"
 
-cmake -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
+REM cmake -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
+cmake --config "%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
 
 exit
 
@@ -16,6 +17,6 @@ echo Usage:
 echo.
 echo generateVisualStudioProject.bat BUILD_TYPE VISUAL_STUDIO_VERSION ARCHITECTURE
 echo.
-echo     BUILD_TYPE: Debug, Release
-echo     VISUAL_STUDIO_VERSION: "Visual Studio 17 2022" or "Visual Studio 16 2019" or older...
-echo     ARCHITECTURE: Win32 x64 ARM ARM64
+echo     BUILD_TYPE: Debug, Release, RelWithDebInfo
+echo     VISUAL_STUDIO_VERSION: "Visual Studio 17 2022", "Visual Studio 16 2019" or older...
+echo     ARCHITECTURE: Win32, x64, ARM, ARM64
