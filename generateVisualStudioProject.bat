7759939708ac20092369000affb4aabbe1553d6d
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2024-06-30 19:18:57 +0200

    Updated VisualStudio generation script.

--- generateVisualStudioProject.bat
+++ generateVisualStudioProject.bat
@@ -7,7 +7,7 @@ set "TARGET_IDE=%~2"
 set "ARCHITECTURE=%~3"
 
 REM cmake -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
-cmake --config "%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
+cmake -DCMAKE_CONFIGURATION_TYPES="%BUILD_TYPE%" -G "%TARGET_IDE%" -A "%ARCHITECTURE%" -S . -B projects/ --fresh
 
 exit
 
